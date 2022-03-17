15750dbc840cd31338dc9774a4c0cae785cdcf50
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-17 17:14:28 +0530

    feat: deleted dropzone component

--- src/components/molecules/DropZone/DropZone.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import DropZone from './DropZone'
-
-describe('DropZone Component', () => {
-  test('DropZone renders', () => {
-    render(<DropZone />)
-  })
-})
