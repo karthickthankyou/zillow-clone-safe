68e037eecaf6869eb152f111e5d958134a7676d2
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-20 23:29:01 +0530

    feat: finished rating stories

--- src/components/molecules/Rating/Rating.stories.tsx
+++ src/components/molecules/Rating/Rating.stories.tsx
@@ -1,16 +1,55 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import Rating from './Rating'
+import NotesFixed from '../NotesFixed'
 
 export default {
   title: 'molecules/Rating',
   component: Rating,
 } as ComponentMeta<typeof Rating>
 
-const Template: ComponentStory<typeof Rating> = (args) => <Rating {...args} />
+const Template: ComponentStory<typeof Rating> = (args) => (
+  <>
+    <Rating {...args} />
+    <NotesFixed>
+      <div>
+        This component is from{' '}
+        <span className='font-bold'>@mui/material/Rating</span> styled with
+        tailwind classes.
+      </div>
+    </NotesFixed>
+  </>
+)
 
-export const Primary = Template.bind({})
-Primary.args = {
-  value: 2.7,
+export const Small = Template.bind({})
+Small.args = {
+  value: 4.5,
 }
-Primary.parameters = {}
+export const Medium = Template.bind({})
+Medium.args = {
+  value: 3.3,
+  size: 'medium',
+}
+export const Large = Template.bind({})
+Large.args = {
+  value: 1.3,
+  size: 'large',
+}
+export const RedLarge = Template.bind({})
+RedLarge.args = {
+  value: 4.1,
+  size: 'large',
+  color: 'red',
+}
+export const BlackMedium = Template.bind({})
+BlackMedium.args = {
+  value: 3.9,
+  size: 'medium',
+  color: 'black',
+}
+export const Disabled = Template.bind({})
+Disabled.args = {
+  value: 1.3,
+  size: 'large',
+  disabled: true,
+}
--- src/components/molecules/Rating/Rating.tsx
+++ src/components/molecules/Rating/Rating.tsx
@@ -1,18 +1,33 @@
-import Rating from '@mui/material/Rating'
+import Rating, { RatingProps } from '@mui/material/Rating'
 
-export interface IRatingProps {
+export type IRatingProps = {
   value: number
+  color?: 'primary' | 'black' | 'red' | 'green' | 'yellow'
+} & RatingProps
+
+const colorClasses = {
+  primary: 'text-primary-600',
+  black: 'text-black',
+  red: 'text-red-600',
+  green: 'text-green-600',
+  yellow: 'text-yellow-600',
 }
 
-const RatingComponent = ({ value }: IRatingProps) => (
+const RatingComponent = ({
+  value,
+  color = 'primary',
+  ...props
+}: IRatingProps) => (
   <Rating
     readOnly
     value={value}
     precision={0.1}
     size='small'
     classes={{
-      iconFilled: 'text-primary-600',
+      iconFilled: colorClasses[color],
     }}
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
   />
 )
 
