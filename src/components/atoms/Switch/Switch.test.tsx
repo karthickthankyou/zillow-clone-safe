3c34196a90fdb3224f743bddf559d38d9ba50b10
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-21 02:03:34 +0530

    feat: finished switch component

--- src/components/atoms/Switch/Switch.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Switch from './Switch'
-
-describe('Switch Component', () => {
-  test('Switch renders', () => {
-    render(<Switch />)
-  })
-})
