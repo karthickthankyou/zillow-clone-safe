25c28c889289397a41f1a7781d34c6251d66759e
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-21 03:26:55 +0530

    chore: deleted unused component FullBanner

--- src/components/organisms/FullBanner/FullBanner.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import FullBanner from './FullBanner'
-
-describe('FullBanner Component', () => {
-  test('FullBanner renders', () => {
-    render(<FullBanner />)
-  })
-})
