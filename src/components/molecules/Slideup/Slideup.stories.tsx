ec6485e73a1e2ab7462341df7c869fae0c14fada
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-21 01:55:32 +0530

    feat: finished slide up component

--- src/components/molecules/Slideup/Slideup.stories.tsx
+++ src/components/molecules/Slideup/Slideup.stories.tsx
@@ -7,10 +7,22 @@ export default {
   component: Slideup,
 } as ComponentMeta<typeof Slideup>
 
-const Template: ComponentStory<typeof Slideup> = (args) => <Slideup {...args} />
+const Template: ComponentStory<typeof Slideup> = (args) => (
+  <>
+    <div className='h-screen90' />
+    <div className='h-screen bg-red-50'>
+      <Slideup {...args} />
+    </div>
+  </>
+)
 
 export const Primary = Template.bind({})
 Primary.args = {
   children: 'Hello World',
 }
-Primary.parameters = {}
+
+export const TriggerPoint50Percentage = Template.bind({})
+TriggerPoint50Percentage.args = {
+  children: 'Hello World',
+  triggerPoint: 0.5,
+}
