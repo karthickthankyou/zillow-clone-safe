1c1e382812d37196d105b0a3845374d6b6a77e92
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-21 00:17:40 +0530

    feat: finished skeleton component

--- src/components/molecules/Skeleton/Skeleton.stories.tsx
+++ src/components/molecules/Skeleton/Skeleton.stories.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Wrapper } from 'src/components/atoms/utils'
 import Skeleton from './Skeleton'
 
 export default {
   title: 'molecules/Skeleton',
   component: Skeleton,
+  decorators: [(story) => <Wrapper>{story()}</Wrapper>],
 } as ComponentMeta<typeof Skeleton>
 
 const Template: ComponentStory<typeof Skeleton> = (args) => (
   <Skeleton {...args} />
 )
 
+const ParagraphTemplate: ComponentStory<typeof Skeleton> = () => (
+  <>
+    <Skeleton className='w-full h-4' />
+    <Skeleton className='w-3/4 h-4 mt-1' />
+    <Skeleton className='w-1/2 h-4 mt-1' />
+  </>
+)
+const UserCardTemplate: ComponentStory<typeof Skeleton> = () => (
+  <div className='flex space-x-3'>
+    <Skeleton className='flex-shrink-0 w-12 h-12' round />
+    <div className='w-full'>
+      <Skeleton className='w-full h-4' />
+      <Skeleton className='w-3/4 h-4 mt-1' />
+      <Skeleton className='w-1/2 h-4 mt-1' />
+    </div>
+  </div>
+)
+
 export const Primary = Template.bind({})
 Primary.args = {
-  className: 'h-24 w-full',
+  className: 'h-4 w-full',
 }
-Primary.parameters = {}
+export const Round = Template.bind({})
+Round.args = {
+  className: 'h-8 w-8',
+  round: true,
+}
+
+export const Paragraph = ParagraphTemplate.bind({})
+export const UserCard = UserCardTemplate.bind({})
--- src/components/molecules/Skeleton/Skeleton.tsx
+++ src/components/molecules/Skeleton/Skeleton.tsx
@@ -1,9 +1,15 @@
 export interface ISkeletonProps {
   className?: string
+  round?: boolean
 }
 
-const Skeleton = ({ className }: ISkeletonProps) => (
-  <div className={`bg-gray-300 animate-pulse ${className}`} />
-)
+const Skeleton = ({ className, round = false }: ISkeletonProps) => {
+  const roundClass = round ? 'rounded-full' : 'rounded'
+  return (
+    <div
+      className={`bg-gray-300 animate-pulse rounded ${roundClass} ${className}`}
+    />
+  )
+}
 
 export default Skeleton
