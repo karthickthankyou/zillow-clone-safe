5d61c74ee06fe2113cab0de98c028b83e17c26bf
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-21 18:02:38 +0530

    finished layout stories

--- src/components/templates/Layout/Layout.stories.tsx
+++ src/components/templates/Layout/Layout.stories.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { SbReduxProvider } from 'src/lib/sb'
 import Layout from './Layout'
 
 export default {
   title: 'templates/Layout',
   component: Layout,
+  decorators: [SbReduxProvider],
 } as ComponentMeta<typeof Layout>
 
 const Template: ComponentStory<typeof Layout> = (args) => <Layout {...args} />
 
 export const Primary = Template.bind({})
-Primary.args = {}
+Primary.args = {
+  children: (
+    <div className='flex items-center justify-center italic text-gray-700 h-screen50 outline-dashed outline-gray-200'>
+      Body content
+    </div>
+  ),
+}
 Primary.parameters = {}
