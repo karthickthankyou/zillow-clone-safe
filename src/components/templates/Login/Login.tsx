b7ba927b0b34ee8e00ad0a90cf5604243122a50e
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-22 16:34:58 +0530

    fix: updated login form for chromatic

--- src/components/templates/Login/Login.tsx
+++ src/components/templates/Login/Login.tsx
@@ -53,7 +53,7 @@ const Login = () => {
       <form onSubmit={onSubmit} className='w-full mt-6 space-y-4'>
         <div>
           <label className='block text-sm text-gray-700'>
-            Email address
+            Email
             <input
               {...register('email')}
               className='block w-full px-3 py-2 mt-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm'
