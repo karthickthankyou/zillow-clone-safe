4a4776a84d6ffea6c719d43f68b467feec605781
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-04-04 17:31:07 +0530

    myaccount page

--- src/components/templates/MyAccount/MyAccount.tsx
+++ src/components/templates/MyAccount/MyAccount.tsx
@@ -1,52 +1,51 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import Container from 'src/components/atoms/Container'
 import { useAppSelector } from 'src/store'
 import { getFunctions, httpsCallable } from 'firebase/functions'
 import { selectUserRoles } from 'src/store/user/userSlice'
 import Button from 'src/components/atoms/Button/Button'
+import { divide } from 'cypress/types/lodash'
 
 export interface IMyAccountProps {}
 
-const MyAccount = () => {
-  const [becoming, setBecoming] = useState(false)
+export const TitleValue = ({
+  title,
+  children,
+}: {
+  title: ReactNode
+  children: ReactNode
+}) => (
+  <div>
+    <div className='font-semibold'>{title}</div>
+    <div>{children}</div>
+  </div>
+)
 
-  const becomeAgentClick = () => {
-    const functions = getFunctions()
-    setBecoming(true)
-    const becomeSellerForZillow = httpsCallable(functions, 'becomeAgent')
-    becomeSellerForZillow().then(() => {
-      setBecoming(false)
-    })
-  }
-  const roles = useAppSelector(selectUserRoles)
+const MyAccount = () => {
+  const user = useAppSelector((state) => state.user)
 
   return (
     <Container className='h-screen '>
-      <div className='mb-2 text-2xl font-semibold'>Agent status</div>
-      <div className='max-w-sm mb-4 text-gray-700'>
-        Agent status will allow you to post your listings and receive leads.
+      <div className='mb-4 text-xl '>My Account</div>
+      <div className='flex flex-col gap-2'>
+        <TitleValue title='Name'>{user.displayName}</TitleValue>
+        <TitleValue title='Email'>{user.email}</TitleValue>
+        <TitleValue title='Roles'>
+          <>
+            {user.roles?.length === 0 ? <div>-</div> : null}
+            <ul>
+              {user.roles?.map((role) => (
+                <li
+                  className='inline-block px-2 py-1 border shadow-lg'
+                  key={role}
+                >
+                  {role}
+                </li>
+              ))}
+            </ul>
+          </>
+        </TitleValue>
       </div>
-      {roles?.includes('agent') ? (
-        <h2>Already an agent!</h2>
-      ) : (
-        <Button
-          isLoading={becoming}
-          className='px-4 py-2 text-white rounded bg-primary-500'
-          onClick={becomeAgentClick}
-        >
-          Become agent
-        </Button>
-      )}
-
-      <div className='mt-6 mb-4 text-2xl font-semibold'>Roles</div>
-      <div>{!roles?.length ? <div>No roles assigned.</div> : null}</div>
-      <ul>
-        {roles?.map((role) => (
-          <li className='inline-block px-2 py-1 border shadow-lg' key={role}>
-            {role}
-          </li>
-        ))}
-      </ul>
     </Container>
   )
 }
